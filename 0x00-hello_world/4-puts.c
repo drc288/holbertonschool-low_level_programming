512dce8215c72c17e1f20f8b067622dc22c9b324
drc288 <davidroserocalle@gmail.com>
2019-06-19 20:20:18 +0000

    my fisrt .c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(){
+/*
+   *
+   *Do not use empty space between example (void){}
+   *puts: print text
+   *
+*/
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
