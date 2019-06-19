0995220bfbcc71cf9c89669c399094dfb472c058
drc288 <davidroserocalle@gmail.com>
2019-06-19 20:53:50 +0000

    point 5 aagain

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - entry point
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle\n");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
