b4b94db739938045ff933dfd218d665db0256685
drc288 <davidroserocalle@gmail.com>
2019-06-20 20:13:10 +0000

    pint 2_0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	int alfa;
+
+	for (alfa = 'a'; alfa <= 'z'; alfa++)
+	{
+		putchar(alfa);
+	}
+
+	return (0);
+}
