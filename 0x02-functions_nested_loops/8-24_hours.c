084ac00fd735683a8941804ca54218840a291ba5
drc288 <davidroserocalle@gmail.com>
2019-06-25 21:38:06 +0000

    point 8_0

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * print_last-digit - print the last
+ * @n: number input
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void jack_bauer(void)
+{
+	int i, j, q, k;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (q = 0; q <=5; q++)
+			{
+				for (k = 0; k <= 9; k++)
+				{
+					_putchar(i % 10 + '0');
+					_putchar(j % 10 + '0');
+					_putchar(58);
+					_putchar(q % 10 + '0');
+					_putchar(k % 10 + '0');
+					_putchar(10);
+				}
+			}
+
+			if ((i == 2) && (j < k))
+			{
+				break;
+			}
+		}
+	}
+}
