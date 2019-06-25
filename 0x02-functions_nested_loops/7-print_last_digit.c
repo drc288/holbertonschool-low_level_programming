7cd8b4cde817103aee640565668b04752616ef1b
drc288 <davidroserocalle@gmail.com>
2019-06-25 20:35:06 +0000

    Point 7_0

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_last-digit - print the last 
+ * @n: number input
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	a = n % 10;
+	if (a < 0)
+	{
+		a = a * -1;
+	}
+	
+	_putchar(a + '0');
+	return (a);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,11 +1,13 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
+int print_last_digit(int);
 
 #endif
+
