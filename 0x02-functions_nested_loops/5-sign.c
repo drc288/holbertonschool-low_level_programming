b60f21fccc3b596a185529347421713cf89c2163
drc288 <davidroserocalle@gmail.com>
2019-06-25 18:34:29 +0000

    point 5_0

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,27 @@
 #include "holberton.h"
+
+/**
+ * print_sign - This is a description
+ */
+
+int print_sign(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		a = 1;
+		_putchar(43);
+	}
+	else if (n < 0)
+	{
+		a = -1;
+		_putchar(45);
+	}
+	else
+	{
+		a = 0;
+		_putchar(48);
+	}
+	return (a);
+}
