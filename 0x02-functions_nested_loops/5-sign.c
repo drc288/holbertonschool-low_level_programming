fab00a9ac0c3c8e7644aa08dd4a16dfcf19b33f0
drc288 <davidroserocalle@gmail.com>
2019-06-25 18:41:02 +0000

    Point 5_0 (betty)

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
 
 /**
- * print_sign - This is a description
+ * print_sign - detect the number - < 0 < +
+ * @n: The character input
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
 int print_sign(int n)
