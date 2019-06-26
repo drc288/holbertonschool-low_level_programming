188e4d1338ea0af964b8818a2e185859b1d1b371
drc288 <davidroserocalle@gmail.com>
2019-06-26 01:08:30 +0000

    Point 9_0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,15 +9,30 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, a;
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i % 10 + '0');
-
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar(i % 10 + '0');
+			a = i * j;
+			if (a <= 9)
+			{
+				if (j != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(a + '0');
+			}
+			else if (a > 9)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(a / 10 + '0');
+				_putchar(a % 10 + '0');
+			}
 		}
 
 		_putchar(10);
