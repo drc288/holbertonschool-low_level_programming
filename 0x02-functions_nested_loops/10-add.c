9fc01cf8c388dae890fe0e2f6922e760f827f714
drc288 <davidroserocalle@gmail.com>
2019-06-26 01:58:54 +0000

    Point 10_0

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - a + b
+ * @a: Input a
+ * @b: Input b
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
