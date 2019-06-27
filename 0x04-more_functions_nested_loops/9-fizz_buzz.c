a3f5045aa24808752b8d879f3ff2975ad707b4d2
drc288 <davidroserocalle@gmail.com>
2019-06-27 23:08:01 +0000

    Point 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	int i, mod5, mod3;
+
+	for (i = 1; i <= 100; i++)
+	{
+		mod3 = i % 3;
+		mod5 = i % 5;
+		if (mod3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (mod5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar(10);
+}
