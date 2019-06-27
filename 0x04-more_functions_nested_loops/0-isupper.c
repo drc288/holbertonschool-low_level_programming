2142a819242361cf003267f2fd3817c33132ed5d
drc288 <davidroserocalle@gmail.com>
2019-06-27 15:44:17 +0000

    Fix 0_1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _isupper - print is upper te value
+ * @c: Input value
+ *
+ * Return: On success 1 is uppercase if not, return 0.
+ */
+
+int _isupper(int c)
+{
+	int i, ret;
+	
+	ret = 0;
+	for (i = 'a'; i < 'z'; i++)
+	{
+		if (c == i)
+		{
+			ret = 1;
+		}
+	}
+
+	return (ret);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme plis :3
