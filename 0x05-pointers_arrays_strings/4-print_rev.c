9bbb5ea82b626bccef044faa7192d3635da94dee
drc288 <davidroserocalle@gmail.com>
2019-07-02 16:15:54 +0000

    point 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * print_rev - print a array charecters reverse
+ * @s: Input data array
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i, count;
+
+	count = _strlen(s);
+	for (i = count; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar(10);
+}
+
+/**
+ * _strlen - Count the maximum number of a array
+ * @s: Input Array
+ *
+ * Return: counter
+ */
+
+int _strlen(char *s)
+{
+	int cont;
+
+	cont = 0;
+	while (s[cont])
+	{
+		cont++;
+	}
+	return (cont);
+}
