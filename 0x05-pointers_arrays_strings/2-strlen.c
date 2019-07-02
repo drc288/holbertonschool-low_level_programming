dc5ead1e1271f90bc42d76f641a481c99edc8147
drc288 <davidroserocalle@gmail.com>
2019-07-02 14:44:15 +0000

    Point 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Count the maximum number of a array
+ * @s: Input Array
+ */
+
+int _strlen(char *s)
+{
+	int i, cont;
+
+	cont = 0;
+	for (i = 0; i < s[i] ; i++)
+	{
+		cont++;
+	}
+	return (cont);
+}
