c0636101ee1bbeccc7c2ca4d5de58545298c0920
drc288 <davidroserocalle@gmail.com>
2019-07-02 21:32:29 +0000

    Point 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - print array
+ * @a: array input
+ * @n: array data []
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i == n)
+		{
+			printf("%d", a[i]);
+			putchar(10);
+		}
+		printf("%d, ", a[i]);
+	}
+}
