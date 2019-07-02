b2292cda7525adb8c6f2f9cb461f162d73059212
drc288 <davidroserocalle@gmail.com>
2019-07-02 15:37:29 +0000

    Fix 3_1

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -17,3 +17,22 @@ void _puts(char *str)
 	}
 	_putchar(10);
 }
+
+/**
+ * _strlen - Count the maximum number of a array
+ * @s: Input Array
+ *
+ * Return: counter
+ */
+
+int _strlen(char *s)
+{
+	int cont;
+
+	cont = 0;
+	while (s[cont])
+	{
+		cont++;
+	}
+	return (cont);
+}
