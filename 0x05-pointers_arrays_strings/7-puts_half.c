d0cfaa17817870758b5c2416d0e61532e594ce56
drc288 <davidroserocalle@gmail.com>
2019-07-02 20:07:28 +0000

    point 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print half of an arrat
+ * @str: Input data array
+ *
+ */
+
+void puts_half(char *str)
+{
+	int cont, mid;
+
+	cont = _strlen(str);
+	mid = cont / 2;
+	while (mid <= cont)
+	{
+		_putchar(str[mid]);
+		mid++;
+	}
+	_putchar(10);
+}
+
+/**
+ * _strlen - Count the maximum number of a array
+ * @s: Input Array
+ *
+ * Return: counter
+ */
+
+int _strlen(char *s)
+{
+	int cont;
+
+	cont = 0;
+	while (s[cont])
+	{
+		cont++;
+	}
+	return (cont);
+}
