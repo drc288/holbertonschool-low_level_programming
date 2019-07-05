afbb082fb8b4f360b5ecab336676f069e9343117
drc288 <davidroserocalle@gmail.com>
2019-07-05 04:51:18 +0000

    point9

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_number - print number integer
+ * @n: number integer
+ * 
+ */
+
+void print_number(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+	if (n / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
