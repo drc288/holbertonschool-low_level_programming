afb60f18795bfd09f90a0ede99ae6f3c31fb9027
drc288 <davidroserocalle@gmail.com>
2019-07-05 04:40:44 +0000

    Pint 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+  * cap_string - Capitalizes all words of a string
+  *@p: string change
+  *
+  *Return: char changed
+  */
+char *cap_string(char *p)
+{
+	int i;
+
+	if(p[0] >= 'a' && p[0] <= 'z')
+	{
+		p[0] = p[0] - 32;
+	}
+	for (i = 1; p[i] != '\0'; i++)
+	{
+		if ((p[i - 1] == ' ' || p[i - 1] == '\t' || p[i - 1] == '\n'
+			|| p[i - 1] == ','
+			|| p[i - 1] == ';' || p[i - 1] == '.' || p[i - 1] == '!'
+			|| p[i - 1] == '?' || p[i - 1] == '"' || p[i - 1] == '('
+			|| p[i - 1] == ')' || p[i - 1] == '{' || p[i - 1] == '}') && (p[i] > 'a' && p[i] < 'z'))
+		{
+			p[i] = p[i] - 32;
+		}
+	}
+	return (p);
+}
