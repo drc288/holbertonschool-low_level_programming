48a88253bcf0db59338722fbf706fd6e75acc040
drc288 <davidroserocalle@gmail.com>
2019-07-08 11:16:12 -0500

    Point 1 :3

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+  * _memcpy - copy src in dest
+  *@dest: dest array
+  *@src: src array
+  *@n: n charecters the src to dest
+  *
+  *Return: char changed
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while ( i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
