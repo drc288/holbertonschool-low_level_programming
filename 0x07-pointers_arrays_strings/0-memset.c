0d311134c5cfdef86bfd5a6bce7ec53a94b0b449
drc288 <davidroserocalle@gmail.com>
2019-07-08 10:52:49 -0500

    Init proyect 0x06 :D!

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+  * _memset - set value in n array
+  *@s: Pointer or buffer
+  *@b: charecter to change
+  *@n: number in total array changes
+  *
+  *Return: char changed
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
