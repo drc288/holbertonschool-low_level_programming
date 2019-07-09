a828aed815a8b8b07c74e1e35b7eb796a04ac7cd
drc288 <davidroserocalle@gmail.com>
2019-07-09 02:23:13 +0000

    Point 5 lets goo!

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,14 +1,33 @@
 #include "holberton.h"
 
 /**
-  * _strchr - Print array to charecter
-  *@s: Array
-  *@c: charecter to print
+  * _strstr - Locate a substring
+  *@haystack: pointer to char
+  *@needle: pointer to char
   *
-  *Return: char changed
+  *Return: char in substring
   */
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i;
+	char **p;
 
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			p = &haystack;
+			for (i = 0; *(needle + i) != '\0'; i++)
+			{
+				if (*(needle + i) != *(haystack + i))
+				{
+					break;
+				}
+				return (*p);
+			}
+		}
+		haystack++;
+	}
+	return (*p);
 }
