7f1f4addd15c9640189346d95a94b286658a6aca
drc288 <davidroserocalle@gmail.com>
2019-07-08 19:08:01 -0500

    adelanto en la casa

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * _strchr - Print array to charecter
+  *@s: Array
+  *@c: charecter to print
+  *
+  *Return: char changed
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+}
