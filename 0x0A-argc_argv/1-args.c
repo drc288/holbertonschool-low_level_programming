04465e544f943463e6c4d7ec0aac89803628d9ec
drc288 <davidroserocalle@gmail.com>
2019-07-13 19:34:10 +0000

    Point 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print number of arguments
+ * @argc: number
+ * @argv: Pointer array
+ *
+ * Return: 0 to success
+ */
+
+int main(int argc, char *argv[])
+{
+	if(*argv)
+	{
+		printf("%d\n", (argc - 1));
+	}
+	return (0);
+}
