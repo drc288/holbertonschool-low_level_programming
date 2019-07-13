46ca9093ce75ef4165d4f624a357cda01bd69637
drc288 <davidroserocalle@gmail.com>
2019-07-13 20:22:50 +0000

    Ready point 2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - mult the 1 to the 3 number
+ * @argc: number
+ * @argv: Pointer array
+ *
+ * Return: 0 to success
+ */
+
+int main(int argc,char *argv[])
+{
+	int mul;
+
+	if (argc == 1 || argc > 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
+	}
+
+	return (0);
+}
