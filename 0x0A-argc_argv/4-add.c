8d9b2c65733a83cb6d7503770647e6e3beff32df
drc288 <davidroserocalle@gmail.com>
2019-07-14 17:14:39 +0000

    Fix point 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - return 1 if thestrings have the number one
@@ -18,17 +19,15 @@ int main(int argc, char *argv[])
 	add = 0;
 	for (i = 1; i < argc; i++)
 	{
-		while (*(*(argv + i) + j) != '\0')
+		for (j = 0; *(*(argv + i) + j) != '\0'; j++)
 		{
-			if (*(*(argv + i) + j) < '0' || *(*(argv + i) + j) > '9')
+			if (isdigit(*(*(argv + i)+ j)) == 0)
 			{
 				printf("Error\n");
 				return (1);
 			}
-			j++;
 		}
-		j = 0;
-		add += atoi(argv[i]);
+		add += atoi(*(argv + i));
 	}
 	printf("%d\n", add);
 	return (0);
