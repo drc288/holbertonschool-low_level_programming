e7c6f35a9c1240fe4b010c18742b131128aac1ca
drc288 <davidroserocalle@gmail.com>
2019-07-16 16:31:05 -0500

    fix 1_6

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -36,22 +36,21 @@ char *_strdup(char *str)
 	int numChar, i;
 	char *p;
 
+	numChar = _strlen_recursion(str);
+	p = malloc((sizeof(char) * numChar) + 1);
+	i = 0;
+
 	if (*str == '\0')
 	{
 		return ('\0');
 	}
-
-	numChar = _strlen_recursion(str) + 1;
-	p = malloc(sizeof(char) * numChar);
-	i = 0;
-
 	if (!p)
 	{
 		return ('\0');
 	}
 	else
 	{
-		while (i <= numChar)
+		while (i <= (numChar - 1))
 		{
 			p[i] = *str;
 			str++;
