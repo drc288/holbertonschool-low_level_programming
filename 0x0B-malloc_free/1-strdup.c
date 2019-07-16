fe16debb1c18a3b0c2357e1921220c378d778265
drc288 <davidroserocalle@gmail.com>
2019-07-16 15:39:36 -0500

    Point 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+
+/**
+ * _strlen_recursion - print # charecters
+ * @s: char caracteres
+ *
+ * Return: # the charecters whit @s
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	n = 0;
+	if (*s != '\0')
+	{
+		n++;
+		s++;
+		return (n + _strlen_recursion(s));
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * _strdup - duplicate a array of char
+ * @str: char to copy
+ *
+ * Return: *pointer copy of str
+ */
+
+char *_strdup(char *str)
+{
+	int numChar, i;
+	char *p;
+
+	numChar = _strlen_recursion(str);
+	p = malloc(sizeof(p) * numChar);
+	i = 0;
+	if (!p)
+	{
+		return ('\0');
+	}
+	if (numChar == 0)
+	{
+		return ('\0');
+	}
+	else
+	{
+		while (i <= numChar)
+		{
+			p[i] = *str;
+			str++;
+			i++;
+		}
+		return (p);
+	}
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,6 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/* Other functions */
+int _strlen_recursion(char *s);
+
+/* Pint 0x08 */
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
