3b40fd5ab457f4b0966e5ed84497f32b33993c5b
drc288 <davidroserocalle@gmail.com>
2019-07-16 14:50:00 -0500

    Fix 0_2

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+/**
+ * create_array - create array using malloc
+ * @size: size of array
+ * @c: charecter
+ *
+ * Return: *pointer with array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p;
+
+	i = 0;
+	p = malloc(sizeof(*p) * size);
+	if (size == 0)
+	{
+		return ('\0');
+	}
+	else
+	{
+		while (i <= size)
+		{
+			p[i] = c;
+			i++;
+		}
+		return (p);
+	}
+}
