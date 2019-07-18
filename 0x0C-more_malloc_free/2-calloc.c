b83eb174802ace62e5edf24626288e39f1602377
drc288 <davidroserocalle@gmail.com>
2019-07-18 16:00:02 -0500

    point 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * _calloc - add in array of nmemb charecters and the size
+ * @nmemb: #of array
+ * @size: size of charecter
+ *
+ * Return: *pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *p;
+
+	i = 0;
+	if ((nmemb == 0) || (size == 0))
+	{
+		return (NULL);
+	}
+
+	p = malloc(size * (nmemb + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < nmemb)
+	{
+		p[i] = 0;
+		i++;
+	}
+
+	p[i] = '\0';
+	return (p);
+}
