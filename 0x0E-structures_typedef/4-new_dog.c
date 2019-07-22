a416f2713d4979276111dfcfb21f35dd624c087a
drc288 <davidroserocalle@gmail.com>
2019-07-22 16:49:31 -0500

    doge

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,43 @@
 #include "dog.h"
 
 /**
+ * _strcpy - copy pointers
+ * @dest: destination
+ * @src: string to copy
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen - Count the maximum number of a array
+ * @s: Input Array
+ *
+ * Return: counter
+ */
+
+int _strlen(char *s)
+{
+	int cont;
+
+	cont = 0;
+	while (s[cont])
+	{
+		cont++;
+	}
+	return (cont);
+}
+/**
  * new_dog - create a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -12,6 +49,9 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *dog_t;
+	char *n;
+	char *o;
+	int i, j;
 
 	dog_t = malloc(sizeof(struct dog));
 	if (dog_t == NULL)
@@ -19,9 +59,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	dog_t->name = name;
+	i = _strlen(name);
+	j = _strlen(owner);
+
+	n = malloc(i + 1);
+	if (n == NULL)
+	{
+		return (NULL);
+	}
+	o = malloc(j + 1);
+	if (o == NULL)
+	{
+		return (NULL);
+	}
+
+	n = _strcpy(n, name);
+	o = _strcpy(o, owner);
+
+	dog_t->name = n;
 	dog_t->age = age;
-	dog_t->owner = owner;
+	dog_t->owner = o;
 
 	return (dog_t);
 }
