f86b22ca7738575f95655c588ad7a84c3854ef38
drc288 <davidroserocalle@gmail.com>
2019-07-24 14:14:09 -0500

    init proyect

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Print the name
+ * @name: name parameter
+ * @f: function toprint the name
+ *
+ * Return: Pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+
+/* PROTOTYPES */
+void print_name(char *name, void (*f)(char *));
+
+#endif
