7f86387181e3ca6b738289e447b0b72da0ebfeaf
drc288 <davidroserocalle@gmail.com>
2019-07-27 14:41:08 -0500

    Fix point 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,46 +1,49 @@
 #include "variadic_functions.h"
-#include <stdio.h>
+
+/**
+ * print_all - Pirnt all caracter according tosign
+ * @format: Format
+ * @...: the signs
+ */
 
 void print_all(const char * const format, ...)
 {
 	va_list li;
-	int i, tam;
+	int i = 0;
 	char *p;
 
-	tam = strlen(format);
-	i = 0;
 	va_start(li, format);
-	while (i < tam)
+	while (format == NULL)
 	{
-		if(format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+		printf("\n");
+		return;
+	}
+	while (format[i] != '\0')
+	{
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(li, int));
-					break;
-				case 'i':
-					printf("%d", va_arg(li, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(li, double));
-					break;
-				case 's':
-					p = va_arg(li, char*);
-					if (p == NULL)
-					{
-						printf("(nil)");
-						break;
-					}
+			case 'c':
+				printf("%c", va_arg(li, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(li, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(li, double));
+				break;
+			case 's':
+				p = va_arg(li, char*);
+				if (p != NULL)
+				{
 					printf("%s", p);
 					break;
-			}
-			while (i != (tam - 1))
-			{
-				printf(", ");
+				}
+				printf("(nil)");
 				break;
-			}
 		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+				format[i] == 's') && format[i + 1] != '\0')
+			printf(", ");
 		i++;
 	}
 	printf("\n");
