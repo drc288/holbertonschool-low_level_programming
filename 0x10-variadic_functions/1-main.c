7a38e8c7432e6fd64e1633341536ec1c96fb846c
drc288 <davidroserocalle@gmail.com>
2019-07-27 12:32:34 -0500

    Fix 1

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_numbers(", ", 4, 0, 98, -1024, 402);
+    print_numbers("-,-", 2, 20, 30);
     return (0);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,7 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list li;
 	unsigned int i;
 
-	if (separator == NULL || n == 0)
+	if (separator == NULL)
 	{
 		return;
 	}
