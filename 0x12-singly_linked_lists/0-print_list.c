ed6dbe7dd13ff3bcb480e40053ae9a705cd1128e
drc288 <davidroserocalle@gmail.com>
2019-08-01 14:09:40 -0500

    Fix point 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@ size_t print_list(const list_t *h)
 {
 	int size = 0;
 
-	while (h->next != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 		{
@@ -24,16 +24,6 @@ size_t print_list(const list_t *h)
 			size++;
 		}
 		h = h->next;
-		if (h->next == NULL)
-		{
-			if (h->str == NULL)
-			{
-				printf("[0] (nil)\n");
-				size++;
-			}
-			printf("[%i] %s\n", h->len, h->str);
-			size++;
-		}
 	}
 	return (size);
 }
