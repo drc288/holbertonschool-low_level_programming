213c2c4640dec8a77d04e5bff8124f0407340387
drc288 <davidroserocalle@gmail.com>
2019-08-06 10:57:19 -0500

    condition in the if

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -51,7 +51,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = *head;
 	total = listint_len(tmp);
 
-	if (idx >= total)
+	if (idx > total)
 	{
 		return (NULL);
 	}
