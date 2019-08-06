2ceeb313ad47ac50936f5695d2222172477ea1fc
drc288 <davidroserocalle@gmail.com>
2019-08-06 10:56:22 -0500

    add function: count nodes and compare with idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * listint_len - Len of the linked list
+ * @h: linked list
+ *
+ * Return: size of linked list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int size;
+
+	size = 0;
+	while (h)
+	{
+		size++;
+		h = h->next;
+	}
+
+	return (size);
+}
+
+/**
  * insert_nodeint_at_index - Insert nodo in idx position
  * @head: Simple list
  * @idx: index
@@ -14,6 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	listint_t *tmp;
 	unsigned int i = 0;
+	size_t total;
 
 	if (head == NULL)
 	{
@@ -27,6 +49,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	tmp = *head;
+	total = listint_len(tmp);
+
+	if (idx >= total)
+	{
+		return (NULL);
+	}
+
 	while (tmp)
 	{
 		if (i == (idx - 1))
