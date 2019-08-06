cf55695a06383f50ba82668ef61253910680b843
drc288 <davidroserocalle@gmail.com>
2019-08-06 14:46:36 -0500

    Iniciando valor de new-n = n al inicio

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,9 +17,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+
+	new->n = n;
 	if (idx == 0)
 	{
-		new->n = n;
 		new->next = *head;
 		*head = new;
 		return (new);
@@ -42,7 +43,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 
-	new->n = n;
 	new->next = tmp->next;
 	tmp->next = new;
 	return (tmp);
