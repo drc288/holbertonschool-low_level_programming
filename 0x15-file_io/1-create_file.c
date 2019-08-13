6e045ae96d319c6df6acaa6c2944c8bb1ca8f211
drc288 <davidroserocalle@gmail.com>
2019-08-13 16:47:05 -0500

    change return value

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, cont;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
