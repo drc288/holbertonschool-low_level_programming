ce06421ee0b347eb6d65717f3f660d7c1586037a
drc288 <davidroserocalle@gmail.com>
2019-08-13 16:44:59 -0500

    change condition text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,8 +32,11 @@ int create_file(const char *filename, char *text_content)
 	char *buf;
 	int fd, cont;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (0);
+
+	if (text_content == NULL)
+		text_content = "";
 
 	cont = _strlen(text_content);
 	buf = malloc(cont);
