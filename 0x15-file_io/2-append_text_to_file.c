b564833f58f0258b8674d14643fb47dd9f2b81bf
drc288 <davidroserocalle@gmail.com>
2019-08-13 19:49:47 -0500

    plis ya

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -34,15 +34,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 	{
 		if (errno == ENOENT || errno == EROFS)
 			return (-1);
 	}
-
-	if (filename == NULL)
-		return (-1);
 
 	if (write(fd, text_content, _strlen(text_content) == -1))
 		return (-1);
