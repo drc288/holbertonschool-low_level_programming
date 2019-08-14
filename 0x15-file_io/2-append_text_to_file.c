6e2fc426c0d246009f1b0f716d6a525b6798d241
drc288 <davidroserocalle@gmail.com>
2019-08-13 19:44:22 -0500

    condition in write

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -44,7 +44,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	write(STDOUT_FILENO, text_content, _strlen(text_content));
+	if (write(fd, text_content, _strlen(text_content) == -1))
+		return (-1);
+
 	close(fd);
 	return (1);
 }
