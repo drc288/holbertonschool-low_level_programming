bdcd8156b461d1df1d5dbcc536e13449b8e0f4fb
drc288 <davidroserocalle@gmail.com>
2019-08-13 19:22:33 -0500

    add point 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Count the maximum number of a array
+ * @s: Input Array
+ *
+ * Return: counter
+ */
+
+int _strlen(char *s)
+{
+	int cont;
+
+	cont = 0;
+	while (s[cont])
+	{
+		cont++;
+	}
+	return (cont);
+}
+
+/**
+ * append_text_to_file - add more text
+ * @filename: name of the file
+ * @text_content: the text
+ *
+ * Return: the file with more text
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+	{
+		if (errno == ENOENT || errno == EROFS)
+			return (-1);
+	}
+
+	if (filename == NULL)
+		return (-1);
+
+	write(fd, text_content, _strlen(text_content));
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,6 +2,7 @@
 #define _HOLBERTON_H_
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 /* OPEN */
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -11,6 +12,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 
 #endif
