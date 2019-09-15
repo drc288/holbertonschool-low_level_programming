330b61f99a7167622aa66a200dd3dbae4f3c9eac
Drose <davidroserocalle@gmail.com>
2019-09-15 18:58:22 +0000

    Add point 5 get index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get the node of index
+ * @head: double link list
+ * @index: the index of nodo
+ *
+ * Return: the double link list in the index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int i = 0;
+
+	tmp = head;
+	while (tmp->next != NULL)
+	{
+		if (i == index)
+			return (tmp);
+		tmp = tmp->next;
+		i++;
+	}
+
+	return (NULL);
+}
