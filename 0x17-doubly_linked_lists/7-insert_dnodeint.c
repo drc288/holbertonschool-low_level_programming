5672a2e6c3bcf09a5ec387b53703248303462195
Drose <davidroserocalle@gmail.com>
2019-09-15 23:58:37 +0000

    add pint 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert a new node in ani position in the node
+ * @h: double linked list
+ * @idx: index of the linked list
+ * @n: value in the new node
+ *
+ * Return: new node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp = NULL, *new = NULL;
+	unsigned int i = 1;
+
+	if (*h == NULL)
+		return (NULL);
+	tmp = *h;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
+	while (tmp != NULL)
+	{
+		if (idx == i)
+		{
+			new->n = n;
+			new->prev = tmp;
+			new->next = tmp->next;
+			tmp->next = new;
+			return (*h);
+		}
+		tmp = tmp->next;
+		++i;
+	}
+	return (NULL);
+}
