c5cd8b1faf079e78f82017073191c0b4424e1ae2
Drose <davidroserocalle@gmail.com>
2019-09-16 00:19:08 +0000

    Fix problem

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - list the nodes of double linked list
+ * @h: the double linked list
+ *
+ * Return: the number of the nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+        const dlistint_t *tmp = NULL;
+        int count = 0;
+
+        if (h == NULL)
+                return (0);
+        tmp = h;
+        while (tmp != NULL)
+        {
+                count += 1;
+                tmp = tmp->next;
+        }
+        return (count);
+}
+
+/**
  * insert_dnodeint_at_index - insert a new node in ani position in the node
  * @h: double linked list
  * @idx: index of the linked list
@@ -12,10 +35,11 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = NULL, *new = NULL;
-	unsigned int i = 1;
+	unsigned int i = 1, n_nodes = 0;
 
 	if (*h == NULL)
 		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -30,6 +54,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new = add_dnodeint(h, n);
 		return (new);
 	}
+
+	n_nodes = dlistint_len(*h);
+	if (idx == n_nodes)
+	{
+		new = add_dnodeint_end(h, n);
+		return (new);
+	}
+
 	tmp = *h;
 	while (tmp != NULL)
 	{
