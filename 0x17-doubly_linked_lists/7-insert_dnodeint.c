be9570d9e77615067e829c4847ce72eff234c995
Drose <davidroserocalle@gmail.com>
2019-09-16 00:01:56 +0000

    fix conditional if idx != 0a

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
-	if (idx == 0)
+	if ((*h == NULL) && (idx != 0))
+		return (NULL);
+	else if (idx == 0)
 	{
 		new = add_dnodeint(h, n);
 		return (new);
