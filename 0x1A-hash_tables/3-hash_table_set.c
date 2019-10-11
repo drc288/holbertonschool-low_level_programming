a7359c6dd2e987aeeb732564ac827c7102795f54
Drose <davidroserocalle@gmail.com>
2019-10-11 22:46:38 +0000

    Fix point, add loop and verify if exists the key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,35 +11,34 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	/* Create index for array and encryot the key */
-	unsigned long int index = 0;
 	unsigned long int array_index = 0;
 	char *dup_key;
 	char *dup_value;
 	hash_node_t *tmp;
 
 	/* Verify if key and value and ht is null */
-	if (key == NULL && value == NULL && ht == NULL)
+	if (key == NULL || value == NULL || ht == NULL)
 		return (0);
 	/* dup values */
 	dup_key = strdup(key);
 	dup_value = strdup(value);
 	/* set index and key_seze */
-	index = ht->size;
-	hash_djb2((unsigned char *)key);
-	array_index = key_index((unsigned char *)key, index);
-	if (array_index > index)
-		return (0);
+	array_index = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[array_index];
-	if (tmp && strcmp(tmp->key, dup_key) == 0)
+	while (tmp != NULL)
 	{
-		tmp->value = dup_value;
+		if (strcmp(tmp->key, dup_key) == 0)
+		{
+			tmp->value = dup_value;
+		}
+		tmp = tmp->next;
 	}
 	tmp = malloc(sizeof(hash_node_t));
 	if (tmp == NULL)
 		return (0);
 	tmp->key = dup_key;
 	tmp->value = dup_value;
-	tmp->next = tmp;
+	tmp->next = ht->array[array_index];
 	ht->array[array_index] = tmp;
 	return (1);
 }
