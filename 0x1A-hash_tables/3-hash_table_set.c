1f565ca6882ed053f5fd50d39bf4f30b29e358cb
Drose <davidroserocalle@gmail.com>
2019-10-11 22:33:51 +0000

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	/* Create index for array and encryot the key */
-	unsigned long int index = 0, i;
+	unsigned long int index = 0;
 	unsigned long int array_index = 0;
 	char *dup_key;
 	char *dup_value;
@@ -22,15 +22,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	/* set index and key_seze */
 	index = ht->size;
-	/* duplicate the key and the value */
-	dup_key = strdup(key);
-	dup_value = strdup(value);
-	/* Create the key */
-	hash_djb2(key);
-	array_index = key_index(key, index);
+	hash_djb2((unsigned char *)key);
+	array_index = key_index((unsigned char *)key, index);
 	if (array_index > index)
 		return (0);
-
+	dup_key = strdup(key);
+	dup_value = strdup(value);
 	tmp = ht->array[array_index];
 	if (tmp && strcmp(tmp->key, dup_key) == 0)
 	{
