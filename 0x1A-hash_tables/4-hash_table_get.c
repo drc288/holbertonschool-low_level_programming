3a1ce544e3a910ed875083f767031e34ca343b00
Drose <davidroserocalle@gmail.com>
2019-10-12 00:20:55 +0000

    fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,12 +12,15 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *tmp;
 
-
 	if (ht == NULL || key == NULL)
 		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[index];
-	if (tmp == NULL)
-		return (NULL);
-	return (tmp->value);
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
