6a73953d0147a1bcb58e4bbaf528575104f74a97
Drose <davidroserocalle@gmail.com>
2019-10-26 22:15:40 +0000

    add proyect

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * 0-bubble_sort - is a simple sorting algorithm that
+ * repeatedly steps through the list
+ * @array: array of integers
+ * @size: size of array
+ * Return: None
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+        size_t i, j;
+        int tmp1;
+
+        for (j = 0; j <= size; j++)
+        {
+                for (i = 1; i <= (size - 1); i++)
+                {
+                        if (array[i - 1] >= array[i])
+                        {
+                                tmp1 = array[i - 1];
+                                array[i - 1] = array[i];
+                                array[i] = tmp1;
+                                print_array(array, size);
+                                continue;
+                        }
+                }
+        }
+}
