62cfa5e487c0de2d6c6627d846dd5184f3ddc97f
Drose <davidroserocalle@gmail.com>
2019-11-15 21:35:44 +0000

    To fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -25,15 +25,15 @@ def island_perimeter(grid):
                 position_x = j
         # Count if grid in position [i][position_x]
         # is equal to 1, it is equal count1++
-        if grid[i][position_x] == 1:
+        if grid[i][position_x - 1] == 0 or position_x == 0:
             count1 += 1
         # verify if grid in position [i][poition_x + 1]
         # is 1, he iterate and count the x part
         if grid[i][position_x + 1] == 1:
             position_y = i
             for k in range(position_x, x):
-                if grid[position_y][k] == 1:
+                if grid[position_y][k] == 1 or position_y == 0:
                     count2 += 1
-    if position_y == 0:
-        return 0
-    return 2 * (count1 + count2)
+    print(count1)
+    print(count2)
+    return count1 + count2
