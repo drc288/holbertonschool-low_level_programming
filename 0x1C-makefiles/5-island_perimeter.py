23ca8f08ab14b36748064efa9644e93334ebbd6d
Drose <davidroserocalle@gmail.com>
2019-11-15 23:08:12 +0000

    time to end

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,29 +9,28 @@ def island_perimeter(grid):
     # Create variables for the activity
     y = len(grid)
     x = len(grid[0])
-    flag = False
-    position_x = 0
-    position_y = 0
-    count1 = 0
-    count2 = 0
+    count = 0
 
     # Iterate the matrix
     for i in range(y):
         for j in range(x):
             # Grid in position [i][j] is equal to 1
             # get the position and set the flag to True
-            if grid[i][j] == 1 and flag is False:
-                flag = True
-                position_x = j
-        # Count if grid in position [i][position_x]
-        # is equal to 1, it is equal count1++
-        if grid[i][position_x - 1] == 0 or position_x == 0:
-            count1 += 1
-        # verify if grid in position [i][poition_x + 1]
-        # is 1, he iterate and count the x part
-        if grid[i][position_x + 1] == 1:
-            position_y = i
-            for k in range(position_x, x):
-                if grid[position_y][k] == 1 or position_y == 0:
-                    count2 += 1
-    return count1 + count2
+            if grid[i][j] == 1:
+                # ------------------ FOR Y ----------------------------
+                # verify if y(i) in the next position are 0 or i are in the last
+                # position
+                if grid[i + 1][j] == 0 or i == (y - 1):
+                    count += 1
+                # verify if the previus position are 0 or
+                # i are in position 0
+                if grid[i - 1][j] == 0 or i == 0:
+                    count += 1
+                # ------------------ FOR X ----------------------------
+                # verify if x(j) in the next position are 0 or j are in the last
+                # position
+                if grid[i][j + 1] == 0 or j == (x - 1):
+                    count += 1
+                if grid[i][j - 1] == 0 or j == 0:
+                    count += 1
+    return count
