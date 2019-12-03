7d4bd1bd8ae67f0d824fa2f3493cff0a8a74d9da
David Rosero <davidroserocalle@gmail.com>
2019-12-02 23:49:12 -0500

    fixing the betty

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -4,46 +4,42 @@
  * binary_tree_insert_left - insert node in the left position
  * @parent: the parent of the node
  * @value: the data of n
- * 
  * Return: the new node
  */
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    /*Create new node*/
-    binary_tree_t *new_node = NULL;
-    /*Using the tmp if parent */
-    binary_tree_t *tmp = NULL;
+	binary_tree_t *new_node = NULL;
+	binary_tree_t *tmp = NULL;
 
-    /*Create space and verify if the node exists*/
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
-    
-    /*Create the data*/
-    new_node->n = value;
-    new_node->right = NULL;
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	/*Create the data*/
+	new_node->n = value;
+	new_node->right = NULL;
 
-    /*Verify if parent in position left exists*/
-    if (parent->left == NULL)
-    {
-        parent->left = new_node; 
-        new_node->parent = parent;
-    }
-    /*Switching the new node with the  tmp node*/
-    else
-    {
-        /*Set the tmp fot the parent*/
-        tmp = parent->left;
-        /**
-         * Switch the parent in the left position with the new node
-         * and switch with the new node with the parent
-         */
-        parent->left = new_node;
-        new_node->parent = parent;
-        new_node->left = tmp;
-        tmp->parent= new_node;
-    }
+	/*Verify if parent in position left exists*/
+	if (parent->left == NULL)
+	{
+		parent->left = new_node;
+		new_node->parent = parent;
+	}
+	/*Switching the new node with the  tmp node*/
+	else
+	{
+		/*Set the tmp fot the parent*/
+		tmp = parent->left;
+		/**
+		 * Switch the parent in the left position with the new node
+		 * and switch with the new node with the parent
+		 */
+		parent->left = new_node;
+		new_node->parent = parent;
+		new_node->left = tmp;
+		tmp->parent = new_node;
+	}
+	return (new_node);
 
-    return (new_node);
 }
