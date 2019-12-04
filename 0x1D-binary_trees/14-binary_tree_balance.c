33c4b7f2c230cd0d8f376921e747288c9a43bbd0
Drose <davidroserocalle@gmail.com>
2019-12-04 20:36:54 +0000

    add point

---
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - get the balance of the node
+ * @tree: the binary tree
+ *
+ * Return: a integer to verify the balance
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+}
