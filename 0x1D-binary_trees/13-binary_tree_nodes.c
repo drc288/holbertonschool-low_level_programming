fba99552cc573e3f75fbc3a8c5c878a5ba350175
Drose <davidroserocalle@gmail.com>
2019-12-04 20:23:57 +0000

    add point

---
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - count the nodes
+ * @tree: the tree
+ *
+ * return the number of nodes
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 1;
+
+	if (tree == NULL)
+		return (0);
+
+	count = binary_tree_nodes(tree->right) +
+		 binary_tree_nodes(tree->left);
+
+	if (tree->left != NULL || tree->right != NULL)
+		return (count + 1);
+
+	return (count);
+}
