65b1406b05a4bf007328e91dd867cc57d57d96f7
Drose <davidroserocalle@gmail.com>
2019-12-04 17:59:47 +0000

    add point

--- 0x1D-binary_trees/10-binary_tree_depth.c
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -1,5 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - 
+ * binary_tree_depth - count in the deph to up
+ * @tree: the tree
+ *
+ * Return: the size
  */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t total = 0;
+
+	if (tree == NULL)
+		return (0);
+	/* if tree->parent exists using rec if not return 0*/
+	total = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
+	return (total);
+}
